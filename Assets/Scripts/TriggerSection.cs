9ba5fcb01dfc44f4dae95bd45996c2329fcb7f75
emrevtnsvr <e.vatansever885@mybvc.ca>
2025-12-08 22:46:33 -0700

    Update

---
+++ Assets/Scripts/TriggerSection.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class TriggerSection : MonoBehaviour
+{
+    public GameObject roadSection;
+
+    private float GetRoadLength()
+    {
+        // Prefabın tüm mesh’lerinin bounding box’ını hesaplar    
+        MeshFilter[] meshes = roadSection.GetComponentsInChildren<MeshFilter>();
+
+        float minZ = float.MaxValue;
+        float maxZ = float.MinValue;
+
+        foreach (MeshFilter mf in meshes)
+        {
+            Bounds b = mf.sharedMesh.bounds;
+            Vector3 worldMin = mf.transform.TransformPoint(b.min);
+            Vector3 worldMax = mf.transform.TransformPoint(b.max);
+
+            minZ = Mathf.Min(minZ, worldMin.z, worldMax.z);
+            maxZ = Mathf.Max(maxZ, worldMin.z, worldMax.z);
+        }
+
+        return Mathf.Abs(maxZ - minZ);  // Gerçek yol uzunluğu
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            float length = GetRoadLength();
+
+            Vector3 spawnPos = transform.root.position + new Vector3(0, 0, -length);
+            Instantiate(roadSection, spawnPos, Quaternion.identity);
+        }
+    }
+}
